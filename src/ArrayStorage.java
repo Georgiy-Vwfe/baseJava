d435fe12fee5df783ddb7db6fab03c96a287fa8a
Georgiy-Vwfe <postnikov.george@yandex.ru>
2018-09-10 12:42:54 +0300

    Created all methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,67 @@
-/**
- * Array based storage for Resumes
- */
+import java.util.Arrays;
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    /**
+     * Array based storage for Resumes
+     */
+    private Resume[] storage = new Resume[10000];
+    private int sizeOfArray = 0;
 
     void clear() {
+        for (int i = 0; i < sizeOfArray; i++) {
+            storage[i] = null;
+        }
+        sizeOfArray = 0;
     }
 
     void save(Resume r) {
+        for (int i = 0; i < sizeOfArray; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                System.out.println("Резюме " + r.uuid + " уже введено");
+                return;
+            }
+        }
+        if (sizeOfArray < storage.length) {
+            storage[sizeOfArray] = r;
+            sizeOfArray++;
+            return;
+        }
+        if (sizeOfArray == storage.length) {
+            System.out.println("Массив полон");
+            return;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < sizeOfArray; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            } else break;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < sizeOfArray; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[sizeOfArray - 1];
+                storage[sizeOfArray - 1] = null;
+                sizeOfArray--;
+                return;
+            } else {
+                System.out.println("Резюме не существует");
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, sizeOfArray);
     }
 
     int size() {
-        return 0;
+        return sizeOfArray;
     }
 }
