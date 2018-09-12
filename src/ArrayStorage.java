c740272982850e99318971b627c9d073e87fc86f
Georgiy-Vwfe <31819579+Georgiy-Vwfe@users.noreply.github.com>
2018-09-12 20:23:25 +0300

    Fixed bugs with get method, delete method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,63 +5,62 @@ public class ArrayStorage {
      * Array based storage for Resumes
      */
     private Resume[] storage = new Resume[10000];
-    private int sizeOfArray = 0;
+    private int sizeOfResume = 0;
 
     void clear() {
-        for (int i = 0; i < sizeOfArray; i++) {
+        for (int i = 0; i < sizeOfResume; i++) {
             storage[i] = null;
         }
-        sizeOfArray = 0;
+        sizeOfResume = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < sizeOfArray; i++) {
+        for (int i = 0; i < sizeOfResume; i++) {
             if (storage[i].uuid.equals(r.uuid)) {
                 System.out.println("Резюме " + r.uuid + " уже введено");
                 return;
             }
         }
-        if (sizeOfArray < storage.length) {
-            storage[sizeOfArray] = r;
-            sizeOfArray++;
+        if (sizeOfResume < storage.length) {
+            storage[sizeOfResume] = r;
+            sizeOfResume++;
             return;
         }
-        if (sizeOfArray == storage.length) {
+        if (sizeOfResume == storage.length) {
             System.out.println("Массив полон");
             return;
         }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < sizeOfArray; i++) {
+        for (int i = 0; i < sizeOfResume; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
-            } else break;
+            }
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < sizeOfArray; i++) {
+        for (int i = 0; i < sizeOfResume; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[sizeOfArray - 1];
-                storage[sizeOfArray - 1] = null;
-                sizeOfArray--;
+                storage[i] = storage[sizeOfResume - 1];
+                storage[sizeOfResume - 1] = null;
+                sizeOfResume--;
                 return;
-            } else {
-                System.out.println("Резюме не существует");
             }
         }
+        System.out.println("Резюме " + uuid + " не существует");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, sizeOfArray);
+        return Arrays.copyOfRange(storage, 0, sizeOfResume);
     }
 
     int size() {
-        return sizeOfArray;
+        return sizeOfResume;
     }
 }
