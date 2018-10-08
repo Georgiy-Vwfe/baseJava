c88454751ea4fec3ed902d446ec8194c75882f28
Georgiy-Vwfe <postnikov.george@yandex.ru>
2018-10-08 13:43:22 +0300

    Create AbstractStorage

--- src/ru/storage/AbstractArrayStorage.java
+++ src/ru/storage/AbstractArrayStorage.java
@@ -8,7 +8,7 @@ import ru.model.Resume;
 import static java.util.Arrays.copyOfRange;
 import static java.util.Arrays.fill;
 
-public abstract class AbstractArrayStorage implements Storage {
+public abstract class AbstractArrayStorage extends AbstractStorage {
 
     protected static final int STORAGE_LIMIT = 10_000;
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
---
+++ src/ru/storage/AbstractStorage.java
@@ -0,0 +1,5 @@
+package ru.storage;
+
+public abstract class AbstractStorage implements Storage {
+
+}
