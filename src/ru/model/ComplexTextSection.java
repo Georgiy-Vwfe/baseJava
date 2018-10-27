cf019820dbf808d10ade29727d89fafaf685f8bb
Georgiy-Vwfe <31819579+Georgiy-Vwfe@users.noreply.github.com>
2018-10-27 12:05:12 +0300

    create ComplexTextSection

---
+++ src/ru/model/ComplexTextSection.java
@@ -0,0 +1,60 @@
+package ru.model;
+
+public class ComplexTextSection {
+    private String label;
+    private String subLabel;
+    private String date;
+    private String title;
+    private String titleDescription;
+
+    public ComplexTextSection(String label, String subLabel, String date, String title, String titleDescription) {
+        this.label = label;
+        this.subLabel = subLabel;
+        this.date = date;
+        this.title = title;
+        this.titleDescription = titleDescription;
+    }
+
+    public String getSubLabel() {
+        return subLabel;
+    }
+
+    public void setSubLabel(String subLabel) {
+        this.subLabel = subLabel;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitleDescription() {
+        return titleDescription;
+    }
+
+    public void setTitleDescription(String titleDescription) {
+        this.titleDescription = titleDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "ComplexTextSection{" +
+                "label='" + label + '\'' +
+                ", subLabel='" + subLabel + '\'' +
+                ", date='" + date + '\'' +
+                ", title='" + title + '\'' +
+                ", titleDescription='" + titleDescription + '\'' +
+                '}';
+    }
+}
